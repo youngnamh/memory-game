8f75d341a394e2f779763a37ad2e11388c391ce5
youngnamh <youngnamhlady@gmail.com>
2023-07-17 16:20:25 -0400

    working pokemon font

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,9 @@ const Header = () => {
     <div className="header">
       <div>
         <h1 id="title">
-          Pokemon Memory Game <img id="pokeball" src={pokeball} />
+          <img id="poke-logo" src={logo} />
+
+          <img id="pokeball" src={pokeball} />
         </h1>
         <p id="instructions">
           Get points by clicking on an imge but don't click on any more than
